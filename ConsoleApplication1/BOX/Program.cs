c1bf7060c0da43e016ba199413289386f5954e8b
Koson Trachu <koson.trachu@gmail.com>
2016-09-07 10:23:41 +0700

    1

---
+++ ConsoleApplication1/BOX/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Box a = new Box(2, 5, 8);
+        Console.WriteLine("Surface Area = {0}", a.SurfaceArea());
+        Console.WriteLine("Volune of box = {0}", a.Volume());
+
+    }
+}
+class Box
+{
+    private int x;
+    private int y;
+    private int z;
+
+    public Box(int x, int y, int z)
+    {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+    public int SurfaceArea()
+    {
+        return (x * y * 2)   // บน + ล่าง
+            + (y * z * 2)    // หน้า + หลัง
+            + x * z * 2;     // ซ้าย + ขวา
+    }
+    public int Volume()
+    {
+        return (x * y * z);     
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        student bird = new student("Bird", 2.5f, 3.0f);
+
+
+    }
+}
+
+public class student
+{
+    private string name;
+    private float gps;
+    private float gpa;
+
+    public student(string name) : this(name, 0, 0)
+    {
+    }
+
+    public student(string name, float gps) : this(name, gps, 0)
+    {
+    }
+    public student(string name, float gps, float gpa)
+    {
+        this.name = name;
+        this.gps = gps;
+        this.gpa = gpa;
+    }
+
+}
